3839f35b758eea991ed8640086b9c8ae5ab63f3b
Mdarafath07 <mdarafathuddin7@gmail.com>
2025-03-24 21:20:42 +0600

    slider back button with error vi fix kore dee T-T

--- app/src/main/java/com/example/ictclubcompact/ImageViewActivity.java
+++ app/src/main/java/com/example/ictclubcompact/ImageViewActivity.java
@@ -2,21 +2,42 @@ package com.example.ictclubcompact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.ictclubcompact.R;
 
-public class ImageViewActivity extends AppCompatActivity {
 
+
+public class ImageViewActivity extends AppCompatActivity {
+    TextView fulligarrow;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_view);
 
+        setContentView(R.layout.activity_image_view);
         ImageView imageView = findViewById(R.id.imageView);
 
         Glide.with(ImageViewActivity.this).load(getIntent().getStringExtra("image")).into(imageView);
+
+
+        fulligarrow = findViewById(R.id.fulligarrow);    //back button
+        fulligarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ImageViewActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
+
